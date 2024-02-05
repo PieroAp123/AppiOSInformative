5e45583f5515465a629852ccd87a1249a36bcdbc
Piero Jhoann Acevedo Pichen <pacevepi@TRJ-YXHD2WVMC2.local>
2024-02-05 13:08:03 -0500

    first commit

---
+++ AppInformative/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppInformative
+//
+//  Created by Piero Jhoann Acevedo Pichen on 5/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
